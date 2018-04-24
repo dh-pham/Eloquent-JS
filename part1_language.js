22d897ea4163106e1349186da1331c9605b6f751
hienhack96 <hpd.hienpham@gmail.com>
2018-04-24 13:35:07 +0700

    ex object

--- part1_language.js
+++ part1_language.js
@@ -1,16 +1,24 @@
 // Chapter 3
 
-// return 0 if even, 1 if odd
-function even_odd_check(num) {
-	if (num <= -1) 
-		return -1;
-	else if (num === 0)
-		return 0;
-	else if (num === 1)
-		return 1;
-	else 
-		return even_odd_check(num - 2);
-}
+// // return 0 if even, 1 if odd
+// function even_odd_check(num) {
+// 	if (num <= -1) 
+// 		return -1;
+// 	else if (num === 0)
+// 		return 0;
+// 	else if (num === 1)
+// 		return 1;
+// 	else 
+// 		return even_odd_check(num - 2);
+// }
 
-console.log(even_odd_check(50));
-console.log(even_odd_check(-2));
+// console.log(even_odd_check(50));
+// console.log(even_odd_check(-2));
+
+// Chapter 4
+
+var description = {
+	work: "whent to work",
+	"touch tree": "touched a tree"
+}
+console.log(description.work);
